478ae0d3782e96274a20a39d3a9b394409c4e6e4
IvayloTsankov <ivaylotsankov93@gmail.com>
2016-02-07 17:39:34 +0200

    * Add 2 default images for processing

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 
 int main(int argc, char *argv[])
 {
-//     if (argc < 2)
-//     {
-//         SLOG("Usage: phi <image_path>");
-//         return (1);
-//     }
+    if (argc < 2)
+    {
+        SLOG("Usage: phi <image_path>");
+        return (1);
+    }
 
-    std::string filename = "images/android.png";
+    //std::string filename = "images/android.png";
+    std::string filename = argv[1];
+
     Image* image = open_image(filename, IMAGE_TYPE::PNG);
     if (! image)
     {
