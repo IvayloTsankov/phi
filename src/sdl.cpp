3ccbf60c7a9d3fb9d9aa6afbaa4eaab15f73efdb
IvayloTsankov <ivaylotsankov93@gmail.com>
2016-02-08 17:52:34 +0200

    * Add function for handling user input

--- src/sdl.cpp
+++ src/sdl.cpp
@@ -1,39 +1,19 @@
-/***************************************************************************
- *   Copyright (C) 2009-2015 by Veselin Georgiev, Slavomir Kaslev et al    *
- *   admin@raytracing-bg.net                                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-/**
- * @File sdl.cpp
- * @Brief Implements the interface to SDL (mainly drawing to screen functions)
- */
 #include <SDL/SDL.h>
 #include <stdio.h>
 #include "sdl.h"
+#include "slog/slog.h"
+
 
 SDL_Surface* screen = NULL;
 
 /// try to create a frame window with the given dimensions
-bool initGraphics(int frameWidth, int frameHeight)
+bool init_graphics(int frameWidth, int frameHeight)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("Cannot initialize SDL: %s\n", SDL_GetError());
 		return false;
 	}
+
 	screen = SDL_SetVideoMode(frameWidth, frameHeight, 32, 0);
 	if (!screen) {
 		printf("Cannot set video mode %dx%d - %s\n", frameWidth, frameHeight, SDL_GetError());
@@ -43,13 +23,13 @@ bool initGraphics(int frameWidth, int frameHeight)
 }
 
 /// closes SDL graphics
-void closeGraphics(void)
+void close_graphics(void)
 {
 	SDL_Quit();
 }
 
 /// displays a VFB (virtual frame buffer) to the real framebuffer, with the necessary color clipping
-void displayVFB(uint32_t* vfb, int width, int height)
+void display_vfb(uint32_t* vfb, int width, int height)
 {
 	int rs = screen->format->Rshift;
 	int gs = screen->format->Gshift;
@@ -82,41 +62,15 @@ void displayVFB(uint32_t* vfb, int width, int height)
 	SDL_Flip(screen);
 }
 
-/// waits the user to indicate he wants to close the application (by either clicking on the "X" of the window,
-/// or by pressing ESC)
-void waitForUserExit(void)
-{
-	SDL_Event ev;
-	while (1) {
-		while (SDL_WaitEvent(&ev)) {
-			switch (ev.type) {
-				case SDL_QUIT:
-					return;
-				case SDL_KEYDOWN:
-				{
-					switch (ev.key.keysym.sym) {
-						case SDLK_ESCAPE:
-							return;
-						default:
-							break;
-					}
-				}
-				default:
-					break;
-			}
-		}
-	}
-}
-
 /// returns the frame width
-int frameWidth(void)
+int frame_width(void)
 {
 	if (screen) return screen->w;
 	return 0;
 }
 
 /// returns the frame height
-int frameHeight(void)
+int frame_weight(void)
 {
 	if (screen) return screen->h;
 	return 0;
