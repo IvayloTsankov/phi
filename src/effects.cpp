8a99303e998193a4d358ed91f6df754904ec3b91
IvayloTsankov <ivaylotsankov93@gmail.com>
2016-02-10 12:42:34 +0200

    * Add new effect

--- src/effects.cpp
+++ src/effects.cpp
@@ -2,28 +2,9 @@
 #include "sdl.h"
 #include "slog/slog.h"
 #include "scale.h"
+#include "utils.h"
 #include <cmath>
 #include <memory>
-
-
-void split_rgb(uint32_t rgbcolor,
-               uint8_t& red, uint8_t& green, uint8_t& blue)
-{
-    red = rgbcolor >> 24;
-    green = rgbcolor >> 16;
-    blue = rgbcolor >> 8;
-}
-
-uint32_t to_rgb(uint8_t red, uint8_t green, uint8_t blue)
-{
-    uint32_t rgb = 0;
-
-    rgb |= uint32_t(red) << 24;
-    rgb |= uint32_t(green) << 16;
-    rgb |= uint32_t(blue) << 8;
-
-    return rgb;
-}
 
 
 void BlackAndWhite(Image* image, SDLKey key, uint8_t type)
@@ -40,9 +21,10 @@ void BlackAndWhite(Image* image, SDLKey key, uint8_t type)
             register uint32_t color = image_buffer[y * width + x];
             uint8_t r, g, b;
             split_rgb(color, r, g, b);
-            register uint16_t brightness = (r + g + b) / 3;
+            uint32_t gray = r * 0.2126 + g * 0.7152 + b * 0.0722;
 
-            color = to_rgb(brightness, brightness, brightness);
+            gray = gray < 255 ? gray : 255;
+            color = to_rgb(gray, gray, gray);
             image_buffer[y * width + x] = color;
         }
     }
@@ -149,3 +131,54 @@ void ScaleDown(Image* image, SDLKey key, uint8_t type)
 }
 
 
+void FloydSteinbergDithering(Image* image, SDLKey key, uint8_t type)
+{
+    int w = image->width;
+    int h = image->height;
+    uint32_t* buffer = image->buffer;
+    for(int y = 0; y < h; ++y)
+    {
+        int error = 0;
+        for(int x = 0; x < w; ++ x)
+        {
+            int pixel_i = (y * w) + x;
+            int right = (y * w) + x + 1;
+            int down = ((y + 1) * w) + x;
+            int rdown = ((y + 1) * w) + x + 1;
+            int ldown = ((y + 1) * w) + x - 1;
+
+            uint32_t color = buffer[pixel_i];
+            uint8_t r, g, b;
+            split_rgb(color, r, g, b);
+            int delta_l = g - r;
+            int delta_r = g - b;
+            
+            if (delta_l <= delta_r)
+            {
+                error += delta_l;
+                color = 0x00000000;
+            }
+            else
+            {
+                error += delta_r;
+                color = 0xFFFFFF00;
+            }
+            
+            buffer[pixel_i] = color;
+
+            // distribute error
+            int error_fraction = error / 16;
+
+            if (x < w)
+                buffer[right] += error_fraction;
+            if (y + 1 < h)
+                buffer[down] += error_fraction;
+            if (y + 1 < h && x + 1 < w)
+                buffer[rdown] += error_fraction;
+            if (y + 1 < h && x - 1 > 0)
+                buffer[ldown] += error_fraction;
+        }
+    }
+
+    display_vfb(image->buffer, image->width, image->height);
+}
--- src/effects.h
+++ src/effects.h
@@ -8,6 +8,8 @@
 void BlackAndWhite(Image* image, SDLKey key, uint8_t type);
 void BrightnessChange(Image* image, SDLKey key, uint8_t type);
 void ScaleDown(Image* image, SDLKey key, uint8_t type);
+void FloydSteinbergDithering(Image* image, SDLKey, uint8_t type);
+void Sepia(Image* image, SDLKey, uint8_t type);
 
 
 #endif // PHI_EFFECTS_H_
